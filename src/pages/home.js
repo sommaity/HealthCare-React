9dc9d366e4ac55cc9b49ddee8ed1fd0487647701
sommaity <sommaity25d@gmail.com>
2024-05-27 10:39:04 +0530

    first commitss

---
+++ src/pages/home.js
@@ -0,0 +1,20 @@
+import './home.css';
+import Navbar from "../Navbar";
+function Home (){
+
+    return (
+    
+        <div>
+            <div className="Apps">
+                <header><Navbar/></header>
+            
+            </div>
+            <div className='home'>
+                <br></br>
+                <p7>Healthcare Inventory Management Systems</p7>
+            </div>
+        </div>
+        );
+
+}
+export default Home;
