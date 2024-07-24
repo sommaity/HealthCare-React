00e510f6e45ace48907b0f6a76fdcdcf3f929352
Suman Maity <50807918+sommaity@users.noreply.github.com>
2024-07-24 17:11:34 +0530

    Update service.js

--- src/Service/service.js
+++ src/Service/service.js
@@ -1,3 +1,4 @@
-const url="https://healthcare-springboot-production.up.railway.app";
+//const url="https://healthcare-springboot-production.up.railway.app";
+const url="https://hismdocker-1.onrender.com";
 //const url="http://localhost:8080"
 export default url;
